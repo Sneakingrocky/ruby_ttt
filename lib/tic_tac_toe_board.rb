54343de088cfc643ebb0b7beed32c1cfad62ea7d
Erica <garciadesign.me@gmail.com>
2014-10-20 22:39:40 -0500

    Updated TicTacToeBoard tests to read board_3x3 and board_4x4 instead of ttt_board1 & 2. Created a method that is able to tell if the game has a winner by using the potential_winning_combos and spaces_occupied_by_one_player methods I created.

--- lib/tic_tac_toe_board.rb
+++ lib/tic_tac_toe_board.rb
@@ -1,7 +1,7 @@
 require 'board'
 
 class TicTacToeBoard < Board
-
+ 
   def rows
     @spaces.each_slice(width).to_a
   end
@@ -18,4 +18,29 @@ class TicTacToeBoard < Board
     (0..width-1).map {|i| rows[i][width-i-1]}
   end
 
+  def combined_diagonals
+    [left_diagonal, right_diagonal]
+  end  
+
+  def pontential_winning_combos
+    rows + columns + combined_diagonals
+  end  
+
+  def game_has_winner?
+    pontential_winning_combos.each do |combo|
+      if spaces_occupied_by_one_player?(combo)
+        return true
+      end  
+    end
+    return false
+  end
+
+  def spaces_occupied_by_one_player?(combo)
+    combo.each do |value|
+      if value != combo.first || value == nil
+        return false  
+      end    
+    end
+    return true  
+  end  
 end
--- spec/tic_tac_toe_board_spec.rb
+++ spec/tic_tac_toe_board_spec.rb
@@ -1,8 +1,8 @@
 require 'tic_tac_toe_board'
 
 describe TicTacToeBoard do
-  let (:ttt_board1) { TicTacToeBoard.new(3) }
-  let (:ttt_board2) { TicTacToeBoard.new(4) }
+  let (:board_3x3) { TicTacToeBoard.new(3) }
+  let (:board_4x4) { TicTacToeBoard.new(4) }
 
 
   describe "create TicTacToe Board" do
@@ -10,15 +10,15 @@ describe TicTacToeBoard do
     describe "rows and columns" do
       
       it "creates rows and columns for a 3x3 board" do
-        ttt_board1.set_space(0, 1)
-        ttt_board1.set_space(1, 2)
-        ttt_board1.set_space(2, 3)
-        ttt_board1.set_space(3, 4)
-        ttt_board1.set_space(4, 5)
-        ttt_board1.set_space(5, 6)
-        ttt_board1.set_space(6, 7)
-        ttt_board1.set_space(7, 8)
-        ttt_board1.set_space(8, 9)
+        board_3x3.set_space(0, 1)
+        board_3x3.set_space(1, 2)
+        board_3x3.set_space(2, 3)
+        board_3x3.set_space(3, 4)
+        board_3x3.set_space(4, 5)
+        board_3x3.set_space(5, 6)
+        board_3x3.set_space(6, 7)
+        board_3x3.set_space(7, 8)
+        board_3x3.set_space(8, 9)
 
 
         expected_output_rows = [[1, 2, 3], 
@@ -31,27 +31,27 @@ describe TicTacToeBoard do
 
 
 
-        expect(ttt_board1.rows).to eq(expected_output_rows)
-        expect(ttt_board1.columns).to eq(expected_output_columns)
+        expect(board_3x3.rows).to eq(expected_output_rows)
+        expect(board_3x3.columns).to eq(expected_output_columns)
       end
       
       it "creates rows and columns for a 4x4 board" do
-        ttt_board2.set_space(0, 1)
-        ttt_board2.set_space(1, 2)
-        ttt_board2.set_space(2, 3)
-        ttt_board2.set_space(3, 4)
-        ttt_board2.set_space(4, 5)
-        ttt_board2.set_space(5, 6)
-        ttt_board2.set_space(6, 7)
-        ttt_board2.set_space(7, 8)
-        ttt_board2.set_space(8, 9)
-        ttt_board2.set_space(9, 10)
-        ttt_board2.set_space(10, 11)
-        ttt_board2.set_space(11, 12)
-        ttt_board2.set_space(12, 13)
-        ttt_board2.set_space(13, 14)
-        ttt_board2.set_space(14, 15)
-        ttt_board2.set_space(15, 16)
+        board_4x4.set_space(0, 1)
+        board_4x4.set_space(1, 2)
+        board_4x4.set_space(2, 3)
+        board_4x4.set_space(3, 4)
+        board_4x4.set_space(4, 5)
+        board_4x4.set_space(5, 6)
+        board_4x4.set_space(6, 7)
+        board_4x4.set_space(7, 8)
+        board_4x4.set_space(8, 9)
+        board_4x4.set_space(9, 10)
+        board_4x4.set_space(10, 11)
+        board_4x4.set_space(11, 12)
+        board_4x4.set_space(12, 13)
+        board_4x4.set_space(13, 14)
+        board_4x4.set_space(14, 15)
+        board_4x4.set_space(15, 16)
        
 
         expected_output_rows = [[1, 2, 3, 4], 
@@ -64,57 +64,108 @@ describe TicTacToeBoard do
                                    [3, 7, 11, 15],
                                    [4, 8, 12, 16]]                           
 
-        expect(ttt_board2.rows).to eq(expected_output_rows)
-        expect(ttt_board2.columns).to eq(expected_output_columns)
+        expect(board_4x4.rows).to eq(expected_output_rows)
+        expect(board_4x4.columns).to eq(expected_output_columns)
       end
     end
 
     describe "left_diagonal" do
       it "returns the spaces in the left diagonal" do
-        ttt_board1.set_space(0, "a")
-        ttt_board1.set_space(4, 2)
-        ttt_board1.set_space(8, 3)
+        board_3x3.set_space(0, "a")
+        board_3x3.set_space(4, 2)
+        board_3x3.set_space(8, 3)
 
         expected_output = ["a", 2, 3]
 
-        expect(ttt_board1.left_diagonal).to eq(expected_output)
+        expect(board_3x3.left_diagonal).to eq(expected_output)
       end
       
       it "returns the spaces in the left diagonal" do
-        ttt_board2.set_space(0, "a")
-        ttt_board2.set_space(5, 2)
-        ttt_board2.set_space(10, 3)
-        ttt_board2.set_space(15, 4)
+        board_4x4.set_space(0, "a")
+        board_4x4.set_space(5, 2)
+        board_4x4.set_space(10, 3)
+        board_4x4.set_space(15, 4)
 
 
         expected_output = ["a", 2, 3, 4]
 
-        expect(ttt_board2.left_diagonal).to eq(expected_output)
+        expect(board_4x4.left_diagonal).to eq(expected_output)
       end
     end
 
     describe "right_diagonal" do
       it "returns the spaces in the right diagonal" do
-        ttt_board1.set_space(2, "a")
-        ttt_board1.set_space(4, 2)
-        ttt_board1.set_space(6, 3)
+        board_3x3.set_space(2, "a")
+        board_3x3.set_space(4, 2)
+        board_3x3.set_space(6, 3)
 
         expected_output = ["a", 2, 3]
 
-        expect(ttt_board1.right_diagonal).to eq(expected_output)
+        expect(board_3x3.right_diagonal).to eq(expected_output)
       end
       
       it "returns the spaces in the right diagonal" do
-        ttt_board2.set_space(3, "a")
-        ttt_board2.set_space(6, 2)
-        ttt_board2.set_space(9, 3)
-        ttt_board2.set_space(12, 4)
+        board_4x4.set_space(3, "a")
+        board_4x4.set_space(6, 2)
+        board_4x4.set_space(9, 3)
+        board_4x4.set_space(12, 4)
 
 
         expected_output = ["a", 2, 3, 4]
 
-        expect(ttt_board2.right_diagonal).to eq(expected_output)
+        expect(board_4x4.right_diagonal).to eq(expected_output)
       end
     end
-  end    
-end
+  
+   describe "game_has_winner?" do
+
+      it "returns true when there are 3 identical tokens in a row" do
+        board_3x3.set_space(0, "x")
+        board_3x3.set_space(1, "x")
+        board_3x3.set_space(2, "x")
+
+        expect(board_3x3.game_has_winner?).to eq(true)
+      end
+
+      it "returns false when there are not 3 identical tokens in a row" do
+        board_3x3.set_space(0, "x")
+        board_3x3.set_space(1, "x")
+        board_3x3.set_space(2, "o")
+
+        expect(board_3x3.game_has_winner?).to eq(false)
+      end
+
+      it "returns true when there are 3 identical tokens in a column" do
+        board_3x3.set_space(0, "o")
+        board_3x3.set_space(3, "o")
+        board_3x3.set_space(6, "o")
+
+        expect(board_3x3.game_has_winner?).to eq(true)
+      end  
+
+      it "returns false when there are not 3 identical tokens in a row" do
+        board_3x3.set_space(0, "o")
+        board_3x3.set_space(3, "x")
+        board_3x3.set_space(6, "o")
+
+        expect(board_3x3.game_has_winner?).to eq(false)
+      end  
+
+      it "returns true when there are 3 identical tokens in a diagonal" do
+        board_3x3.set_space(0, "x")
+        board_3x3.set_space(4, "x")
+        board_3x3.set_space(8, "x")
+
+        expect(board_3x3.game_has_winner?).to eq(true)
+      end  
+
+      it "returns false when there are not 3 identical tokens in a diagonal" do
+        board_3x3.set_space(0, "o")
+        board_3x3.set_space(4, "x")
+        board_3x3.set_space(8, "x")
+
+        expect(board_3x3.game_has_winner?).to eq(false)
+      end  
+    end    
+  end
+end  
