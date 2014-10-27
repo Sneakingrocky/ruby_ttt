0daad425a37fbcd6b112ba408f6ee92140f4cb46
Erica <garcialuck08@gmail.com>
2014-10-26 23:12:30 -0500

    Validating human player's move

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -2,10 +2,25 @@ class HumanPlayer < Player
   def get_move(board)
     ask_for_move
   
-    @ui.get_input.to_i
+    move = @ui.get_input.to_i
+
+    if valid_move?(move, board)
+      move
+    else
+      display_move_error
+      get_move(board)
+    end
   end
 
   def ask_for_move
     @ui.display("#{name}, what is your next move?")
   end  
+
+  def display_move_error
+    @ui.display("Space is not available. Please choose a different space.")
+  end
+
+  def valid_move?(move, board)
+    board.available_spaces.include?(move)
+  end
 end  
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,24 +1,43 @@
+require 'player'
+require 'tic_tac_toe_board'
 require 'human_player'
 require 'tic_tac_toe_ui'
 
 describe "Human Player" do
+  let(:ui) { TicTacToeUi.new }
+  let(:player) { HumanPlayer.new("player1", "X", ui) }
+  let(:board) { TicTacToeBoard.new(3) }
+
+  before do
+    allow(ui).to receive(:display)
+    allow(ui).to receive(:get_input)
+  end
 
   describe "get_move" do
     it "gets the players move" do
-      ui = TicTacToeUi.new
-      player = HumanPlayer.new("player1", "X", ui)
-
       allow(ui).to receive(:get_input) { "6" }
 
-      expect(player.get_move).to eq(6)
+      expect(player.get_move(board)).to eq(6)
     end  
     
     it "asks for next move" do
-      ui = TicTacToeUi.new
-      player = HumanPlayer.new("player1", "X", ui)
-
       expect(ui).to receive(:display).with("player1, what is your next move?")
-      player.get_move
+      player.get_move(board)
+    end
+
+    context "bad move" do
+      it "asks for the player's move again" do
+        allow(ui).to receive(:get_input).and_return("20", "6")
+
+        expect(player.get_move(board)).to eq(6)
+      end
+
+      it "tells the player the move is invalid" do
+        allow(ui).to receive(:get_input).and_return("20", "6")
+        expect(ui).to receive(:display).with("Space is not available. Please choose a different space.")
+
+        player.get_move(board)
+      end
     end
   end
 end
